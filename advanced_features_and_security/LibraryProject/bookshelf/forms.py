56e32096daf0a5e3e2ef086ba88b925bb5f056d7
Ali Abdelhakam <aliabdo96@gmail.com>
2024-11-17 00:07:40 +0300

    module 11 q 2 attempt 3

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,15 +1,7 @@
 # LibraryProject/bookshelf/forms.py
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date', 'isbn', 'genre']
-
-    # Custom validation can be added here if necessary
-    def clean_isbn(self):
-        isbn = self.cleaned_data.get('isbn')
-        if len(isbn) != 13:  # Example validation for ISBN length
-            raise forms.ValidationError('ISBN must be 13 characters long')
-        return isbn
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
