ed7b20f20c9ebd63e6c9d73eae9c047bb13fef63
Ali Abdelhakam <aliabdo96@gmail.com>
2024-11-16 23:07:35 +0300

    MODULE 10 Q 4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     # Retrieve all books from the database
@@ -27,14 +28,6 @@ class LibraryDetailView(DetailView):
         context['books'] = self.object.books.all()
         return context
 
-# Login view
-#class CustomLoginView(LoginView):
-#    template_name = 'relationship_app/login.html'
-
-# Logout view
-#class CustomLogoutView(LogoutView):
-#    template_name = 'relationship_app/logout.html'
-
 # Registration view
 def register(request):
     if request.method == 'POST':
@@ -46,3 +39,28 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# Helper functions to check roles
+def is_admin(user):
+    return user.is_authenticated and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.profile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.profile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
