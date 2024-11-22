6c4b3dafc2628dbc26e596c5a6686b0c234a54ee
Ali Abdelhakam <aliabdo96@gmail.com>
2024-11-22 18:48:03 +0300

    project 12 q 3

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # Display fields in the admin list view
+    list_display = ('title', 'author')
+
+    # Add list filters for easy filtering
+    list_filter = ('title', 'author')
+
+    # Enable search capabilities
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r"books", BookViewSet)
+router.register(r"books_all", BookViewSet, basename="book_all")
 
 
 urlpatterns = [
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     path("books/", BookList.as_view(), name="book_list"),
     path("", include(router.urls)),
 ]
